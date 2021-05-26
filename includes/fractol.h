378e57881029e8ff713bfc6b0f19bc6d8a39a990
lburnet <lburnet@student.42lyon.fr>
2021-05-26 13:53:51 +0200

    push before lecture

--- includes/fractol.h
+++ includes/fractol.h
@@ -6,7 +6,7 @@
 /*   By: lburnet <lburnet@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/26 13:31:55 by lburnet           #+#    #+#             */
-/*   Updated: 2021/05/26 13:34:36 by lburnet          ###   ########lyon.fr   */
+/*   Updated: 2021/05/26 13:52:53 by lburnet          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,4 +18,36 @@
 # include "stdio.h"
 # include "mlx.h"
 
+/* Structs for fract-ol ***************************************************** */
+
+typedef struct s_vars	t_vars;
+typedef struct s_data	t_data;
+typedef struct s_rgb	t_rgb;
+
+struct s_vars
+{
+	void	*win;
+	void	*mlx;
+	t_data	*img;
+};
+
+struct s_data
+{
+	void	*img;
+	char	*addr;
+	int		bits_per_pix;
+	int		line_len;
+	int		endian;
+};
+
+struct s_rgb {
+	int		i;
+	int		t;
+	int		b;
+	int		g;
+	int		r;
+	float	fb;
+	float	fg;
+	float	fr;
+};
 #endif
--- scrs/main.c
+++ scrs/main.c
@@ -6,8 +6,13 @@
 /*   By: lburnet <lburnet@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/26 13:31:41 by lburnet           #+#    #+#             */
-/*   Updated: 2021/05/26 13:36:11 by lburnet          ###   ########lyon.fr   */
+/*   Updated: 2021/05/26 13:53:02 by lburnet          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
+
+int	main(int ac, char *av[])
+{
+	
+}
